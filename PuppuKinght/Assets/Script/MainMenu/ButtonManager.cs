391ff86b0be72b31ed3d6cb0ea59f32c1be0f262
hungngo3012 <hungngo189@gmail.com>
2022-06-15 16:14:59 +0700

    15/06/2022

---
+++ PuppuKinght/Assets/Script/MainMenu/ButtonManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadGame()
+    {
+
+    }
+
+    public void Setting()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
